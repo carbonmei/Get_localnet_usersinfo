b0ffd2b48436ffa361c0c2d0d28a5ed60a0932bd
carbonmei <34277424+carbonmei@users.noreply.github.com>
2022-04-23 15:30:58 +0800

    Add files via upload
    fix logical bug,have optimized code architecture

--- main_pro.py
+++ main_pro.py
@@ -24,7 +24,7 @@ def generate_csv(group):
     :param group: 爬取过程中储存的数组
     """
     netdata = pd.DataFrame(group)
-    netdata.to_csv("net_userinfo.csv", mode='a')
+    netdata.to_csv("netinfo_0423.csv", mode='a')
 
 
 def update_file(file, old_str, new_str):
@@ -44,11 +44,11 @@ def update_file(file, old_str, new_str):
         f.write(file_data)
 
 
-def get_mac(file_path, i, j):
+def get_mac(file_path, j, j_end, i_start, i_end):
     """
     自动采集器
     :param file_path:
-    :param i: IP第四位起始数值，请与shell文件值相同
+    :param i_start: IP第四位起始数值，请与shell文件值相同
     :param j: IP第三位起始数值，请与shell文件相同
     :return:
     """
@@ -65,49 +65,43 @@ def get_mac(file_path, i, j):
             headers = {'user-agent': 'Mozilla/5.0'}
             reg = requests.get(url=url_net, headers=headers).text
             soup = BeautifulSoup(reg, 'html.parser')
-            netinfo = {}
             userId = soup.find(id="userId")['value']
         except (requests.exceptions.ConnectionError, AttributeError, requests.exceptions.ChunkedEncodingError):
             print("获取速度过快，服务器遭不住啦，重试中......")
             continue
-        # ----------每循环一次就会调用文本更改函数更新IP值----------------
         file = file_path
-        old_str = f"rand_num={i}"
-        new_str = f"rand_num={i + 1}"
+        old_str = f"rand_num={i_start}"
+        new_str = f"rand_num={i_start + 1}"
         old_strs = f"rand_nu={j}"
         new_strs = f"rand_nu={j + 1}"
-        old_str1 = "rand_num=256"
+        old_str1 = f"rand_num={i_end}"  # 与第四位IP终止值相同
         new_str1 = "rand_num=1"  #
-        i += 1
+        i_start += 1
         update_file(file, old_str, new_str)
-        if j > 38:  # 第三位IP终止值
+        if j > j_end:  # 第三位IP终止值
             break
-        if userId == "":
-            # ---------值为空不抓取---------------
+        if i_start == i_end:  # 与第四位IP终止值相同
+            generate_csv(group)  # 生成一次文件
+            group = []
+            update_file(file, old_str1, new_str1)  # 重置第四位IP
+            i_start = 0
+            update_file(file, old_strs, new_strs)  # 第三位IP进一位
+            j += 1
+        if userId == "":  # ---------值为空不抓取---------------
             print("信息为空，重试中。。。。。。")
             continue
-        netinfo['userId'] = userId
-        netinfo['wlanuserip'] = soup.find(id="wlanuserip")['value']
-        netinfo['mac'] = soup.find(id="mac")['value']
-        netinfo['macs'] = soup.find(id="mac")['value'].replace(":", "")
-        netinfo['tip '] = soup.find(class_="succ-content").text.replace('\n', '')
+        netinfo = {'userId': userId, 'wlanuserip': soup.find(id="wlanuserip")['value'],
+                   'mac': soup.find(id="mac")['value'], 'macs': soup.find(id="mac")['value'].replace(":", ""),
+                   'tip ': soup.find(class_="succ-content").text.replace('\n', '')}
         # ---------获取用户名字信息-------------
         url_netName = "http://1.1.1.1:8888/self/toChangePassword.do?accountId=" + userId
         regs = requests.get(url=url_netName, headers=headers).text
         soup = BeautifulSoup(regs, 'html.parser')
         netinfo['Name'] = soup.find(id="accountName")['value']
         # --------------调用函数把循环后的字典添加进数组中储存----------------
-        group.append(netinfo)
-        print(f"已执行{i}次")
-        if i > 255:  # 第四位IP终止值
-            update_file(file, old_str1, new_str1)  # 重置第四位IP
-            i = 0
-        if i == 255:
-            update_file(file, old_strs, new_strs)  # 第三位IP进一位
-            j += 1
-            generate_csv(group)
+        group.append(netinfo)  # 每end一次添加一次字典
         print(f"已成功获取到{len(group)}条数据")
 
 
 file_path = "internet.cmd"  # shell文件路径
-get_mac(file_path, 2, 33)   # 调用函数，路径，分别是第四位和第三位IP起始值
+get_mac(file_path, 32, 38, 2, 255)  # 调用函数，路径，分别是第三位和第四位IP起始值,终止值
